e927985d95a00dbbcffc3fe6d3c60aa18a51b31c
Varga Marcell <computing8063@mailbox.unideb.hu>
2025-09-23 21:19:30 +0200

    fixed relative view folder declaration, changed it to start from project dir

--- server.mjs
+++ server.mjs
@@ -1,11 +1,15 @@
 import express from "express";
 import { Search } from "./ImageScraperService.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", __dirname + "/views");
 app.use(express.static("static"));
 
 app.get("/", (_, res) => {
